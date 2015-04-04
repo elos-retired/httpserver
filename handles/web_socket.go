1104376639ff4ae8ee50b90107e0d3f7e9297a33
Nick Landolfi <nclandolfi@gmail.com>
2015-04-04 10:14:22 -0700

    refactor websocket handlers

--- handles/web_socket.go
+++ handles/web_socket.go
@@ -12,23 +12,22 @@ import (
 )
 
 func WebSocket(u transfer.WebSocketUpgrader, connMan autonomous.Manager) AccessHandle {
-	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params, a data.Access) {
-		conn, err := u.Upgrade(w, r, a.Client())
-
-		if err != nil {
-			log.Printf("An error occurred while upgrading to the websocket protocol, err: %s", err)
-			// gorilla.websocket will handle response to client
-			return
-		}
-
-		log.Printf("Agent with id %s just connected over websocket to ClientData", a.Client().ID())
-
+	return WebSocketUpgrade(u, func(conn transfer.SocketConnection, a data.Access) {
 		agent := agents.NewClientDataAgent(conn, a)
 		connMan.StartAgent(agent)
-	}
+	})
 }
 
 func REPL(u transfer.WebSocketUpgrader, connMan autonomous.Manager) AccessHandle {
+	return WebSocketUpgrade(u, func(conn transfer.SocketConnection, a data.Access) {
+		agent := agents.NewREPLAgent(conn, a)
+		connMan.StartAgent(agent)
+	})
+}
+
+type WebSocketSuccess func(transfer.SocketConnection, data.Access)
+
+func WebSocketUpgrade(u transfer.WebSocketUpgrader, success WebSocketSuccess) AccessHandle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params, a data.Access) {
 		conn, err := u.Upgrade(w, r, a.Client())
 
@@ -40,7 +39,6 @@ func REPL(u transfer.WebSocketUpgrader, connMan autonomous.Manager) AccessHandle
 
 		log.Printf("Agent with id %s just connected over websocket to REPL", a.Client().ID())
 
-		agent := agents.NewREPLAgent(conn, a)
-		connMan.StartAgent(agent)
+		success(conn, a)
 	}
 }
